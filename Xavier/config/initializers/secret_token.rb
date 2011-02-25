e85ec3e2b5c6872c90cece0213e8e94d7d3109bd
Alvaro Pereyra <alvaro@xendacentral.com>
2011-02-25 11:22:32 -0500

    Removes Xavier folder from list

--- Xavier/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Xavier::Application.config.secret_token = '128f37b40a307af144279f5a51ba135c71f3e5e46012ba92643307f18051a52b3d29ad5262c7a5add2bceb6722e362e1c9282f61813b4a405c49240d8218494f'
