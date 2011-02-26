e05e90d97f96cb8418c2784bdb4a785b9e6a94a8
Alvaro Pereyra <alvaro@xendacentral.com>
2011-02-25 23:22:54 -0500

    Fixes duplication code by refactoring the products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 	
-	before_filter :authenticate_user!, :only => {:add_to_cart, :purchase}
+	before_filter :authenticate_user!, :only => [:add_to_cart, :purchase]
 	
 	def show
 		@product = Product.find(params[:id])
@@ -8,20 +8,19 @@ class ProductsController < ApplicationController
 	
 	def add_to_cart
 		@product = Product.find(params[:product_id])
-		
-		@purchase = Purchase.new
-		@purchase.product_id = @product.id
-		@purchase.user_id = current_user.id
-		@purchase.status = 'pending'
+	  @purchase = @product.purchases.build :user_id => current_user.id, :status => 'pending'	
 		
 		if @purchase.save
 			redirect_to '/cart'
+    else
+      flash[:warning] = "There was an issue while processing your cart"
+      render :show
 		end
 	end
 	
 	def purchase
 		@product = Product.find(params[:product_id])
-		@purchase = Purchase.new(:product_id => @product.id)
+    @purchase = @product.purchases.create
 	end
 	
 end
