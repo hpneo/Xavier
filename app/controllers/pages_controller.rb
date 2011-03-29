35309bceae0e0a0d4deb8867db09b0036e3ce957
HPNeo <hpneo@hotmail.com>
2011-03-29 11:06:48 -0500

    Mostrar cursos en 'our courses', 'best sellers', 'free courses' y 'categories'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,16 @@ class PagesController < ApplicationController
 	
 	def show
 		@page = Page.find(params[:id])
+		case @page.id
+			when 18: #ourcourses
+				@courses = Product.courses
+			when 24: #free courses
+				@courses = Product.courses.free
+			when 15: #bestsellers
+				@courses = Product.courses.popular
+			when 17: #categories
+				@categories = Category.all
+		end
 	end
 	
 end
