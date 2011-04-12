bc77a80a4cf016922c03b9de6a60faa18672896a
Alvaro Pereyra <alvaro@xendacentral.com>
2011-04-12 18:07:45 -0500

    Adds host to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,4 +48,5 @@ Xavier::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => 'abc-onlinelearningcenter.com' }
 end
