1c0bbd5b62bd1aacb5df4bb6e8efd88bdaebfc2c
Alvaro Pereyra <alvaro@xendacentral.com>
2011-04-12 17:25:07 -0500

    Banners will show on their language in home

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -9,6 +9,14 @@ class Slide < ActiveRecord::Base
 		:storage => :s3,
 		:s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
 		:path => '/slide/spanish/:id/:filename'
+		
+	def self.spanish_slides
+	 Slide.where("spanish_source_file_name IS NOT NULL")
+	end
+	
+	def self.english_slides
+	 Slide.where("source_file_name IS NOT NULL")	 
+	end
 	
 	def file
 		self.source
