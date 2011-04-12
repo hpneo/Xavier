e9c8c028d9a652a62e77be2c6f6603ed12bc98b0
Alvaro Pereyra <alvaro@xendacentral.com>
2011-04-12 17:56:05 -0500

    Adds confirmation_token to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110412224634) do
+ActiveRecord::Schema.define(:version => 20110412225527) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(:version => 20110412224634) do
     t.datetime "updated_at"
     t.string   "role",                                :default => "user"
     t.datetime "confirmed_at"
+    t.string   "confirmation_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
