ac0853c5dcbd59996dd7c1dd4b62dfc6e84ab964
Alvaro Pereyra <alvaro@xendacentral.com>
2011-04-15 22:37:57 -0500

    Setups default cookie language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :check_cookie
+  
 	def authenticate_admin!
   		redirect_to root_path unless current_user && authenticate_user! && current_user.admin?
+	end
+	
+	def check_cookie
+	 cookies[:language] ||= "en"
 	end
 end
