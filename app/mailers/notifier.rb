fee442a0a5fc0e8498a699ebde35b6f6221c5374
HPNeo <hpneo@hotmail.com>
2011-07-04 17:35:12 -0500

    Cambiar destinatario de notificaciones

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
 	
 	def support_notification(sender)
 		@sender = sender
-		mail(:to => 'admin@abc-onlinelearningcenter.com', :from => sender.email, :subject => 'New #{sender.support_type}')
+		mail(:to => 'contact@abc-onlinelearningcenter.com', :from => sender.email, :subject => 'New #{sender.support_type}')
 	end
 	
 end
